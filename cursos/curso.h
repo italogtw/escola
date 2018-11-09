de2db20bc3d10ad3f31a99cbaa7c43c949f301b4
italogtw <44896634+italogtw@users.noreply.github.com>
2018-11-09 09:24:08 -0200

    Update curso.h

--- cursos/curso.h
+++ cursos/curso.h
@@ -4,5 +4,17 @@ typedef struct {
 
 
 }Curso;
+void ordenaCursosInsertionsort(CrudCurso *c){
+    int aux, i, j;
+    for(i=0;i<c->qtdAtual -1;i++){
+        for(j=i+1;j<c->qdtAtual;j++){
+            if(strcmp(c->cursos[j].nome,c->cursos[i])<0){
+                aux=c->cursos[j].nome;
+                c->cursos[j].nome=c->cursos[i];
+                c->cursos[i]=aux;
+            }
+        }
+    }
+}
 
 #endif // CURSO_H
